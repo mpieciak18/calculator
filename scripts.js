95b2ad07ff9670555ebedc5db5979ae5acf5fbc4
Mark Pieciak <mpieciak18@msn.com>
2021-02-18 22:44:04 -0500

    design basic HTML & CSS

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let add = function(x,y) {
+    return (x + y)
+};
+let subtract = function(x,y) {
+    return (x - y)
+};
+let multiply = function(x,y) {
+    return (x * y)
+};
+let divide = function(x,y) {
+    return (x / y)
+};
+let operator = function(x,y,operation) {
+    return operation(x,y);
+}
