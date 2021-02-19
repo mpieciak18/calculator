7e82164a45ea46b1deec9446b151062e6eb186ae
Mark Pieciak <mpieciak18@msn.com>
2021-02-18 23:22:28 -0500

    add hover & click styling to buttons

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
+// let buttons = document.getElementsByClassName('button');
+
+// let hoverColor = function(event) {
+//     let hoveredButton = event.target;
+//     hoveredButton.setAttribute('style', 'background-color: black');
+// };
+// let clickColor = function(event) {
+//     let clickedButton = event.target;
+//     clickedButton.setAttribute('style', 'background-color: green');
+// };
+// let unclickColor = function(event) {
+//     let hoveredButton = event.target;
+//     hoveredButton.setAttribute('style', 'background-color: red');
+// };
+// let unhoverColor = function(event) {
+//     let clickedButton = event.target;
+//     clickedButton.setAttribute('style', 'background-color: white');
+// };
+// for (i = 0; i < buttons.length; i++) {
+//     console.log(buttons[i]);
+//     buttons[i].addEventListener('mousedown', clickColor);
+//     // buttons[i].addEventListener('mouseover', hoverColor);
+//     buttons[i].addEventListener('mouseup', unclickColor);
+//     // buttons[i].addEventListener('mouseout', unhoverColor);
+// };
 let add = function(x,y) {
     return (x + y)
 };
