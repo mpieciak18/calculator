43c301490e8aea3adf9f9c902786d8f53b5cc01f
Mark Pieciak <mpieciak18@msn.com>
2021-02-19 21:48:39 -0500

    make :hover & :active desktop-only and add eventListener for touchstart & touchend on mobile

--- scripts.js
+++ scripts.js
@@ -1,28 +1,33 @@
-// let buttons = document.getElementsByClassName('button');
-
-// let hoverColor = function(event) {
-//     let hoveredButton = event.target;
-//     hoveredButton.setAttribute('style', 'background-color: black');
-// };
-// let clickColor = function(event) {
-//     let clickedButton = event.target;
-//     clickedButton.setAttribute('style', 'background-color: green');
-// };
-// let unclickColor = function(event) {
-//     let hoveredButton = event.target;
-//     hoveredButton.setAttribute('style', 'background-color: red');
-// };
-// let unhoverColor = function(event) {
-//     let clickedButton = event.target;
-//     clickedButton.setAttribute('style', 'background-color: white');
-// };
-// for (i = 0; i < buttons.length; i++) {
-//     console.log(buttons[i]);
-//     buttons[i].addEventListener('mousedown', clickColor);
-//     // buttons[i].addEventListener('mouseover', hoverColor);
-//     buttons[i].addEventListener('mouseup', unclickColor);
-//     // buttons[i].addEventListener('mouseout', unhoverColor);
-// };
+let buttons = document.getElementsByClassName('button');
+let touchColor = function(event) {
+    let clickedButton = event.target;
+    if (clickedButton.id == 'ac') {
+        clickedButton.style.backgroundColor = 'rgb(206, 164, 164)';
+    } else if (clickedButton.id == 'c') {
+        clickedButton.style.backgroundColor = 'rgb(206, 183, 159)';
+    } else if (clickedButton.id == 'equals') {
+        clickedButton.style.backgroundColor = 'rgb(160, 207, 160)';
+    } else {
+        clickedButton.style.backgroundColor = 'rgb(201, 201, 201)';
+    };
+};
+let untouchColor = function(event) {
+    let hoveredButton = event.target;
+    if (hoveredButton.id == 'ac') {
+        hoveredButton.style.backgroundColor = 'rgb(255, 200, 200)';
+    } else if (hoveredButton.id == 'c') {
+        hoveredButton.style.backgroundColor = 'rgb(255, 228, 200)';
+    } else if (hoveredButton.id == 'equals') {
+        hoveredButton.style.backgroundColor = 'rgb(200, 255, 200)';
+    } else {
+        hoveredButton.style.backgroundColor = 'rgb(255, 255, 255)';
+    };
+};
+for (i = 0; i < buttons.length; i++) {
+    console.log(buttons[i]);
+    buttons[i].addEventListener('touchstart', touchColor);
+    buttons[i].addEventListener('touchend', untouchColor);
+};
 let add = function(x,y) {
     return (x + y)
 };
