581a0732eac76018889ce885b5abd19f900c63e3
Mark Pieciak <mpieciak18@msn.com>
2021-02-20 12:07:54 -0500

    add event listener & dummy functions to remaining buttons

--- scripts.js
+++ scripts.js
@@ -24,17 +24,20 @@ let untouchColor = function(event) {
     };
 };
 let screenDisplay = document.getElementById('numbers');
+let firstNumber = '';
+let secondNumber = '';
+let enteringFirstNumber = true;
 let enterNumber = function(event) {
     let newDisplay;
     if (screenDisplay.innerHTML == '0') {
         newDisplay = event.target.innerHTML;
-    } else if (screenDisplay.innerHTML.indexOf('.') == -1 && screenDisplay.innerHTML.length < 9) {
-        newDisplay = screenDisplay.innerHTML + event.target.innerHTML;
-    } else if (screenDisplay.innerHTML.indexOf('.') != -1 && screenDisplay.innerHTML.length < 10) {
+    } else if (screenDisplay.innerHTML.indexOf('.') == -1 && screenDisplay.innerHTML.length < 9
+        || screenDisplay.innerHTML.indexOf('.') != -1 && screenDisplay.innerHTML.length < 10) {
         newDisplay = screenDisplay.innerHTML + event.target.innerHTML;
     } else {
         newDisplay = screenDisplay.innerHTML;
-    }
+    };
+    updateInputs(newDisplay);
     screenDisplay.innerHTML = newDisplay;
 };
 let enterDecimal = function(event) {
@@ -45,18 +48,15 @@ let enterDecimal = function(event) {
         newDisplay = screenDisplay.innerHTML;
     };
     screenDisplay.innerHTML = newDisplay;
-}
-for (i = 0; i < buttons.length; i++) {
-    console.log(buttons[i]);
-    buttons[i].addEventListener('touchstart', touchColor);
-    buttons[i].addEventListener('touchend', untouchColor);
-    if (Number(buttons[i].id) >= 0 && Number(buttons[i].id) <= 9) {
-        buttons[i].addEventListener('click', enterNumber);
-    } else if (buttons[i].id == 'decimal') {
-        console.log(buttons[i].id);
-        buttons[i].addEventListener('click', enterDecimal);
-    }
 };
+let updateInputs = function(newDisplay) {
+    if (enteringFirstNumber == true) {
+        firstNumber = newDisplay;
+    } else {
+        secondNumber = newDisplay;
+    };
+};
+// Placeholder operator functions
 let add = function(x,y) {
     return (x + y)
 };
@@ -69,6 +69,45 @@ let multiply = function(x,y) {
 let divide = function(x,y) {
     return (x / y)
 };
-let operator = function(x,y,operation) {
-    return operation(x,y);
-}
+let percent = function(x,y) {
+    return (x / y)
+};
+let clear = function(x,y) {
+    x = '';
+    y = '';
+};
+let allClear = function(x,y) {
+    x = '';
+    y = '';
+};
+let operator = function() {
+    return;
+};
+
+// Add eventListener + functions to each button
+for (i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    button.addEventListener('touchstart', touchColor);
+    button.addEventListener('touchend', untouchColor);
+    if (Number(button.id) >= 0 && Number(button.id) <= 9) {
+        button.addEventListener('click', enterNumber);
+    } else if (button.id == 'decimal') {
+        button.addEventListener('click', enterDecimal);
+    } else if (button.id == 'equals') {
+        button.addEventListener('click', operator);
+    } else if (button.id == 'multiply') {
+        button.addEventListener('click', multiply);
+    } else if (button.id == 'minus') {
+        button.addEventListener('click', subtract);
+    } else if (button.id == 'plus') {
+        button.addEventListener('click', add);
+    } else if (button.id == 'divide') {
+        button.addEventListener('click', divide);
+    } else if (button.id == 'percent') {
+        button.addEventListener('click', percent);
+    } else if (button.id == 'c') {
+        button.addEventListener('click', clear);
+    } else if (button.id == 'ac') {
+        button.addEventListener('click', allClear);
+    };
+};
